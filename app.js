4de08f3114b773508031127b36d57269bbe03b38
Akash Roshan <akash.second1999@gmail.com>
2020-10-02 17:36:27 +0530

    test ejs views

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 const express = require("express");
 const app = express();
 
+// view engine, renders views
+app.set("view engine", "ejs");
+
+// routes
 app.get("/", (req, res) => {
-    res.send("Hello World!!");
+    res.render("index");
 });
 
+// app listener
 app.listen(3000, () => console.log("App is running"));
